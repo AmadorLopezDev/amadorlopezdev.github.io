d8149f7cd819cd706f6d5a9a9fa8fdb02851cde3
AmadorLopezDev <amador.lopez.dev@outlook.es>
2025-01-13 19:14:19 +0000

    reboot

--- src/config.ts
+++
@@ -1,3 +0,0 @@
-//agrega una constante llamada Site_Title
-export const Site_Title = "Página personal de Amador López Parra";
-export const Site_Description = "Esta página personal está basada en el template <a href='https://astrofy-template.netlify.app/'>Astrofy</a> y contiene información sobre mí a nivel profesional."
