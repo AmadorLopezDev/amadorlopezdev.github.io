436f76441091a4f8d03a373adf056bff6e3025b4
AmadorLopezDev <amador.lopez.dev@outlook.es>
2025-02-20 15:27:04 +0000

    Agregado RSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+    site: 'https://amadorlopezdev.github.io',
     integrations: [tailwind()]
 });
