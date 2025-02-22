6453c9f5694593e411460e053fc9841dc23b86d7
AmadorLopezDev <amador.lopez.dev@outlook.es>
2025-02-22 19:40:07 +0000

    add poder oculto

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import dotenv from 'dotenv';
+import remarkHtml from 'remark-html';
+import remarkImages from 'remark-images';
 
 dotenv.config();
 
 export default defineConfig({
     site: 'https://amadorlopezdev.github.io',
-    integrations: [tailwind()]
+    integrations: [tailwind()],
+    markdown: {
+        remarkPlugins: [
+            [remarkImages, {className: 'responsive-image'}],
+            remarkHtml,
+        ]
+    },
 });
