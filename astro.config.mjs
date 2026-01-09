8f13f9934534bf1e461b21de5843a76221c0e1ec
Amador López Parra <amador.lopez.dev@outlook.es>
2026-01-09 14:02:16 +0000

    update astro and package versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import dotenv from 'dotenv';
 import remarkHtml from 'remark-html';
 import remarkImages from 'remark-images';
-
-dotenv.config();
 
 export default defineConfig({
     site: 'https://amadorlopezdev.github.io',
